eeea0dee1101a92b0abe2d54392d4253a4cf5ea3
armandoaliaga <armandoaliaga92@gmail.com>
2013-08-22 23:24:26 -0400

    segundo commit

---
+++ app/controllers/cruci_controller.rb
@@ -0,0 +1,5 @@
+class CruciController < ActionController::Base
+ def index
+   
+ end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crucigrama::Application.config.secret_token = '93974e704edcbd79f766261e67e748538f351f8506ccbec26c63cd75cee9931a5e6b6d06c101b315f123fdb95107c6235a4c881520415748960eed2e0797d552'
